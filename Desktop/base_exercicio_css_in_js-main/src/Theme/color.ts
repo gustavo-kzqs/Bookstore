94853e1eb2de0bcbc23bc29eea07fa56985bc15e
gustavo-kzqs <gustavo.kazeker@gmail.com>
2025-09-24 15:29:35 -0300

    Delete Desktop/base_exercicio_css_in_js-main directory

--- Desktop/base_exercicio_css_in_js-main/src/Theme/color.ts
+++
@@ -1,7 +0,0 @@
-const Color = {
-  corPrincipal: '#a7727d',
-  corSecundaria: '#f9f5e7',
-  corTextoHero: '#eee'
-}
-
-export default Color
--- Desktop/base_exercicio_css_in_js-main/src/components/Cabecalho/index.tsx
+++
@@ -1,9 +0,0 @@
-import { CabecalhoH } from './styles'
-
-const Cabecalho = () => (
-  <CabecalhoH>
-    <h1>EBAC Jobs</h1>
-  </CabecalhoH>
-)
-
-export default Cabecalho
